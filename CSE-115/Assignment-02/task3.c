fc6e7ff9ba4646b54b37833256ea2f55e2ed4dfb
Habib1ur <habiburrahman.1.chowdhury@gmail.com>
2025-02-13 01:45:30 +0600

    updated

--- CSE-115/Assignment-02/task3.c
+++ CSE-115/Assignment-02/task3.c
@@ -5,12 +5,25 @@ int main()
     char x;
     printf("Input an Alphabet: ");
     scanf("%c", &x);
-    if (x>='A' && x<='Z' || x>='a' && x<='z' )
+    if (x >= 'A' && x <= 'Z' || x >= 'a' && x <= 'z')
     {
-        printf("Next character is: %c",x+1);
-    }else{
+        if (x == 'z')
+        {
+            printf("Next character is: a");
+        }
+        else if (x == 'Z')
+        {
+            printf("Next character is: A");
+        }
+        else
+        {
+            printf("Next character is: %c", x + 1);
+        }
+    }
+    else
+    {
         printf("ERORR! Please input a alphabet.");
     }
-    
+
     return 0;
 }
